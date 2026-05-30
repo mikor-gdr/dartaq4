65b9a8e2f3244c778a01f0980003e3a595f8a83c
mikor resta <abdurrochimgdr@gmail.com>
2026-05-30 13:39:36 +0700

    edit siswa

---
+++ app/api/siswa/route.js
@@ -0,0 +1,15 @@
+import { createConnection } from "@/lib/db.js";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const db = await createConnection();
+    const sql = "SELECT * FROM siswa";
+    const [posts] = await db.query(sql);
+    return NextResponse.json({ post: posts });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error.message });
+  }
+}
+ 
